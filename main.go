5f3ebd91ba43b97f22e59fa1f8b6866be41b6f6e
usamahussain <[200~usamahussaincs@gmail.com>
2023-10-05 04:11:12 +0500

    created basic web server using standard library net/http

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main()  {
+	// define the server. here & represents that we storing the value as a pointer by taking the
+	// memory address (reference type) rather than the value (value type)
+	server := &http.Server{
+		// inside here we can define our server properties like
+		Addr: ":3000", // port
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("failed to listen to server", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
