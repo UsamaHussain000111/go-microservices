65863e73e5bbdd94315deaf327a0894a51509596
usamahussain <[200~usamahussaincs@gmail.com>
2023-10-15 01:01:00 +0500

    chi router and middleware added

--- main.go
+++ main.go
@@ -3,15 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	//add a chi router
+	router := chi.NewRouter()
+	//use the logging for routes
+	router.Use(middleware.Logger)
+
+	router.Get("/hello", basicHandler)
 	// define the server. here & represents that we storing the value as a pointer by taking the
 	// memory address (reference type) rather than the value (value type)
 	server := &http.Server{
 		// inside here we can define our server properties like
 		Addr:    ":3000", // port
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
