cea5b5bb87fd5bfb0357e8d7f572cdfa438a257e
usamahussain <[200~usamahussaincs@gmail.com>
2023-10-15 23:16:07 +0500

    redis and graceful shutdown implemented0

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/UsamaHussain000111/go-microservices/app"
 )
@@ -10,7 +12,10 @@ import (
 func main() {
 	app := app.New()
 
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 
 	if err != nil {
 		fmt.Println("failed to start the app:", err)
